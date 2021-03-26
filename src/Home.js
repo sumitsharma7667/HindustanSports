967169b3a5bc99a9a3821ba76117f6de1e4d1a8c
unknown <sharmaankit6407158@gmail.com>
2021-03-26 10:50:22 +0530

    new update

--- src/Home.js
+++ src/Home.js
@@ -51,6 +51,7 @@ const Home = () => {
             // console.log(data[3].data.image[0])
         })
     }
+    
     if (data[0] !== undefined) {
         return (
 
@@ -59,7 +60,7 @@ const Home = () => {
                     <div className="container-fluid headercontainer">
                         <div className="row">
                             <div className="col-2 mt-2">
-                                <img src={require("./images/logo.png").default} className="logo" />
+                                <img src={require("./images/logo (1).png").default} className="logo" />
                             </div>
                             <div className="col-md-6 navUlC">
                                 <ul className="navMenulist">
@@ -72,11 +73,19 @@ const Home = () => {
                                 </ul>
                             </div>
                             <div className="col-md-4">
+                                 <div className="row">
                                 <ul className="navMenulist">
-                                    <li>Login/Register</li>
-                                    <li>Wishlist</li>
-                                    <li><i class="fas fa-shopping-cart"></i>Cart</li>
+                                        <div className="col-4">
+                                            <li>Login/Register</li>
+                                        </div>
+                                        <div className="col-4">
+                                            <li>Wishlist</li>
+                                        </div>
+                                        <div className="col-4">
+                                            <li>Cart <i class="fas fa-shopping-cart"></i></li>
+                                            </div>
                                 </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -85,26 +94,45 @@ const Home = () => {
                     <div className="container-fluid">
                         <div className="row">
                             <div className="col-md-2">
-                                <select className="form-control homeCategory" name="cars" id="cars">
+                                <div class="dropdown">
+                                    <span class="dropdown__btn">Categories</span>
+                                    <a href="#" class="dropdown__link ">Shoes</a>
+                                    <a href="#" class="dropdown__link ">Shoes</a>
+                                    <a href="#" class="dropdown__link ">Shoes</a>
+                                    <a href="#" class="dropdown__link ">Shoes</a>
+                                    <a href="#" class="dropdown__link ">Shoes</a>
+                                </div>
+                                {/* <select className="form-control homeCategory" name="cars" id="cars">
                                     <option value="">Categories</option>
-                                </select>
+                                </select> */}
+                                
                             </div>
+                            
                             <div className="col-md-5 IconsSec">
-                                <div className="row">
+                                <div className="row iconsHead">
                                     <div className="col-6">
                                         <h5>Follow us on </h5>
                                     </div>
                                     <div className="col-6">
-                                        <i class="fab fa-twitter"></i>
-                                        <i class="fab fa-facebook"></i>
-                                        <i class="fab fa-instagram"></i>
+                                        <div className="row">
+                                            <div className="col-4">
+                                                <i class="fab fa-twitter"></i>
+                                            </div>
+                                            <div className="col-4">
+                                                <i class="fab fa-facebook"></i>
+                                            </div>
+                                            <div className="col-4">
+                                                <i class="fab fa-instagram"></i>
+                                                </div>
+                                            </div>
                                     </div>
                                 </div>
                             </div>
                             <div className="col-md-5">
-                                <div>
-                                    <input type="text" placeholder="Search.." name="search" />
-                                    <button type="submit"><i class="fa fa-search"></i></button>
+                                <div className="search">
+                                   <input type="text" class="searchTerm" placeholder="What are you looking for?"/>
+                                    <button type="submit" class="searchButton">
+                                        <i class="fa fa-search"></i></button>
                                 </div>
                             </div>
                         </div>
@@ -112,43 +140,43 @@ const Home = () => {
                 </section>
                 <section className="imagesSect mt-3">
                     <div className="container">
-                        <div className="row">
-                            <div className="col-md-6 ImagesClas">
-                                <img src={data[3].data.image[0]} className="" />
+                        <div className="row imageRow">
+                            <div className="col-md-6   ">
+                                <img src={data[3].data.image[0]} className="w-100  mb-4 firstrowImg"/>
                             </div>
-                            <div className="col-md-6">
-                                <div className="row">
-                                    <div className="col-md-6 ImagesClas">
-                                        <img src={data[4].data.image[0]} className="MaskGroup" />
+                            <div className="col-md-6 colSixsec">
+                                <div className="row seconRow">
+                                    <div className="col-md-6  ">
+                                        <img src={data[4].data.image[0]} className="w-100 s mb-4 firstrowImg imgClas" />
                                     </div>
-                                    <div className="col-md-6 ImagesClas">
-                                        <img src={data[3].data.image[1]} className="MaskGroup" />
+                                    <div className="col-md-6  ">
+                                        <img src={data[3].data.image[1]} className="  mb-4 firstrowImg imgClasSec" />
+                                    </div>
+                                     </div>
+                                <div className="row seconRow">
+                                    <div className="col-md-6  ">
+                                        <img src={data[3].data.image[2]} className="w-100  mb-4 firstrowImg imgClas" />
+                                    </div>
+                                    <div className="col-md-6 ">
+                                        <img src={data[4].data.image[0]} className=" mb-4 firstrowImg imgClasSec" />
                                     </div>
                                 </div>
-                                <div className="row">
-                                    <div className="col-md-6 ImagesClas">
-                                        <img src={data[3].data.image[2]} className="MaskGroup" />
-                                    </div>
-                                    <div className="col-md-6 ImagesClas">
-                                        <img src={data[4].data.image[0]} className="MaskGroup" />
-                                    </div>
-                                </div>
                             </div>
-                        </div>
+                      </div>
                         <div className="row">
-                            <div className="col-3 ImagesClas">
-                                <img src={require("./images/cricketer.png").default} className="MaskGroup" />
+                            <div className="col-3  ">
+                                <img src={require("./images/cricketer.png").default} className="  mb-4  ImagesClas cricketimg" />
                             </div>
-                            <div className="col-3 ImagesClas">
-                                <img src={require("./images/kit.jpg").default} className="MaskGroup" />
+                            <div className="col-3  ">
+                                <img src={require("./images/kit.jpg").default} className="w-100  mb-4 ImagesClas" />
                             </div>
-                            <div className="col-6 lastCol ImagesClas">
-                                <img src={require("./images/18-SM268387.jpg").default} className="lastCol" />
+                            <div className="col-6 lastCol   ">
+                                <img src={require("./images/18-SM268387.jpg").default} className="w-100  mb-4 ImagesClas imgClas" />
                             </div>
                         </div>
                     </div>
                 </section >
-                <section className=" thirdSec mt-5">
+                <section className=" thirdSec">
                     <div className="headCat"><h4>Categories</h4></div>
 
                     <hr />
@@ -249,7 +277,6 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
-                <hr />
                 <div className="container">
                     <div className="row mt-5">
                         <div className="col-md-3">
@@ -364,6 +391,90 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
+
+                {/* Footer Section */}
+                <footer className="footSect">
+                    <div className="container">
+                        <div className="row mt-3">
+                            <div className="col-3 footCol">
+                                <h4>ABOUT US</h4>
+                                <span>
+                                <p>Who we are</p>
+                                 <p>Decathalon CSR</p>
+                                <p>Careers</p>
+                                <p>News Room</p>
+                                    <p>Made in India</p>
+                                    </span>
+                            </div>
+                            <div className="col-3 footCol">
+                                <h4>SUPPORT</h4>
+                                <span>
+                                <p>Conctact Our Stores</p>
+                                    <p>Delivery</p>
+                                    </span>
+                            </div>
+                            <div className="col-3 footCol">
+                                <h4>LEGAL</h4>
+                                <spna>
+                                <p>Return Policy</p>
+                                 <p>Terms and Conditions</p>
+                                    <p>Privacy Policy</p>
+                                    </spna>
+                                
+                            </div>
+                            <div className="col-3 footCol">
+                                 <h4>Connect with Us</h4>
+                                <p>0987886676</p>
+                                <div className="footerIcon">
+                                <i class="fab fa-twitter iCoNSS"></i>
+                                <i class="fab fa-facebook iCoNSS"></i>
+                                    <i class="fab fa-instagram iCoNSS"></i>
+                                    </div>
+                             </div>
+
+                        </div>
+                        <div className="row">
+                            <div className="col-3 footCol">
+                                <div className="addressL">
+                                <h4>Address</h4>
+                                <span>
+                                <p>22, Connought Place</p>
+                                <p>Dehradun Uttarakhand</p>
+                                    <p>248001 India</p>
+                                    </span>
+                                    </div>
+                            </div>
+                        
+                        <div className="col-9 ">
+                            <div className="row mt-4">
+                                <div className="col-3">
+                                     <img src={require("./images/image-8.png").default} className="" />
+                                </div>
+                                <div className="col-3">
+                                    <img src={require("./images/image-9.png").default}/>
+                                </div>
+                                    <div className="col-3">
+                                        <img src={require("./images/image-11.png").default} />
+                                    </div>
+                                    <div className="col-3">
+                                        <img src={require("./images/image-12.png").default} />
+                                </div>
+                                </div>
+                                <div className="row mt-2">
+                                <div className="col-3">
+                                     <img src={require("./images/image-15.png").default} className="" />
+                                </div>
+                                <div className="col-3">
+                                    <img src={require("./images/image-13.png").default}/>
+                                    </div>
+                                     <div className="col-3">
+                                    <img src={require("./images/image-10.png").default}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </footer>
             </>
         )
     }
