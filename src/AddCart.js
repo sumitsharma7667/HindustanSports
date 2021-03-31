83ded5fb7bafd321b570f122c3ba41440dc74c33
sumit <sumit.sharma@giksindia.com>
2021-03-31 18:13:47 +0530

    sdf

--- src/AddCart.js
+++ src/AddCart.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import firebaseDb from "./firebase"
-
+import Header from "./Header"
 const Addcart = () => {
     var totalamount=0
     useEffect(() => {
@@ -18,44 +18,10 @@ const Addcart = () => {
             })
         })
     }
-
     if (data !== undefined) {
         return (
             <React.Fragment>
-                <header>
-                    <div className="container-fluid headercontainer">
-                        <div className="row">
-                            <div className="col-2 mt-2">
-                                <img src={require("./images/logo (1).png").default} className="logo" />
-                            </div>
-                            <div className="col-md-6 navUlC">
-                                <ul className="navMenulist">
-                                    <li>Home/</li>
-                                    <li> Categories/</li>
-                                    <li> Best Sellers/</li>
-                                    <li> New Release/</li>
-                                    <li> Shop by Brand/</li>
-                                    <li> Featured</li>
-                                </ul>
-                            </div>
-                            <div className="col-md-4">
-                                <div className="row">
-                                    <ul className="navMenulist">
-                                        <div className="col-4">
-                                            <li>Login/Register</li>
-                                        </div>
-                                        <div className="col-4">
-                                            <li>Wishlist</li>
-                                        </div>
-                                        <div className="col-4">
-                                            <li>Cart <i class="fas fa-shopping-cart"></i></li>
-                                        </div>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </header>
+                <Header/>
                 <section class="card sectCard">
                     <div className="container-fluid">
                         <div className="row">
--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import $ from 'jquery';
 import firebaseDb from "./firebase"
 import "./style.css"
+import Header from "./Header"
 // import Carousel from "react-multi-carousel";
 // import "react-multi-carousel/lib/styles.css";
 const responsive = {
@@ -58,49 +59,7 @@ const Home = () => {
     if (data[0] !== undefined) {
         return (
             <>
-                <header>
-                    <div className="container-fluid headercontainer">
-                        <div className="row">
-                            <div className="col-2 mt-2">
-                                <img src={require("./images/logo (1).png").default} className="logo" />
-                            </div>
-                            <div className="col-md-6 navUlC">
-                                <ul className="navMenulist">
-                                    <li>Home/</li>
-                                    <li> Categories/</li>
-                                    <li> Best Sellers/</li>
-                                    <li> New Release/</li>
-                                    <li> Shop by Brand/</li>
-                                    <li> Featured</li>
-                                </ul>
-                            </div>
-                            <div className="col-md-4">
-                                 <div className="row">
-                                <ul className="navMenulist">
-                                        <div className="col-4 d-flex">
-                                            <li>
-                                                <Link to="Login">
-                                                Login/
-                                                </Link>
-                                                </li>
-                                            <li>
-                                                <Link to="signup">
-                                                Register
-                                                </Link>
-                                                </li>
-                                        </div>
-                                        <div className="col-4">
-                                            <li>Wishlist</li>
-                                        </div>
-                                        <div className="col-4">
-                                            <li>Cart <i class="fas fa-shopping-cart"></i></li>
-                                            </div>
-                                </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </header>
+                <Header/>
                 <section class="card sectCard">
                     <div className="container-fluid">
                         <div className="row">
